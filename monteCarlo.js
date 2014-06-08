700a405b7383cf3e06b330f91f7c701b707ed5f2
Mark Smith <msmith@rallydev.com>
2014-06-08 09:16:40 -0400

    Brute force method that breaks when targetScope is over 15.

--- monteCarlo.js
+++ monteCarlo.js
@@ -1,5 +1,5 @@
 $(function () {
-
+	debugger;
 	var calcVelocities = function(burnUp){
 		var velocities;
 		if(burnUp == null || burnUp.length === 0){
@@ -29,116 +29,193 @@ $(function () {
 		return iterations;
 	};
 
-	var burnUp = [1, 5, 9, 15, 18];
+	// var burnUp = [1, 5, 9, 15, 18];
+	var burnUp = [1, 5];
 
 	var velocities = calcVelocities(burnUp);
+	console.log("velocities = ", velocities);
 
 	var maxIterations = 20;
 	var iterations = createXAxisLabels(maxIterations);
 
-	var noRuns = 500;
-	var targetScope = 40
+	var noRuns = 100;
+	var targetScope = 15;
 
-	var projectionRoot = {
+	projectionRoot = {
 		x: burnUp.length - 1,
 		y: burnUp[burnUp.length -1],
 		remaining: noRuns,
-		parent: null,
 		children: []
 	};
 
 	var endCounts = {};
-	var pathEnds = [];
-	var toProcess = [ projectionRoot ];
 
-	while(toProcess.length > 0){
-		var current = toProcess.pop();
+	var series = [
+		{
+			data: burnUp,
+			name: 'Actual'
+		}
+	];
+
+	var nextSeries = 1;
+
+	var processNode = function(parentSeries, node, createNewSeries){
+		// console.log("processing node ("+ node.x +", "+ node.y+"), parentSeries.name = "+ parentSeries.name+
+		// 	", createNewSeries =  "+ createNewSeries);
+		var pathSeries;
+		if(createNewSeries){
+			// var show = nextSeries < 3;
+			var show = true;
+			pathSeries = {
+				data: parentSeries.data.slice(0),
+				name: 'Path '+ nextSeries++,
+				dashStyle: 'Dash',
+				color: '#FF8000',
+				marker: {
+					symbol: 'circle'
+				},
+				visible: show,
+				showInLegend: show
+
+			};
+			series.push(pathSeries);
+			// console.log("creating path "+ pathSeries.name);
+		}
+		else{
+			pathSeries = parentSeries;
+		}
+
+		pathSeries.data.push({
+			x: node.x,
+			y: node.y
+		});
+
+
+		// figure out children
+		if(node.y < targetScope){
+			for(var i=0, l=velocities.length; i < l; i++){
+				var newY = node.y + velocities[i];
+				if(newY > targetScope){
+					newY = targetScope;
+				}
+				var child = {
+					x: node.x +1,
+					y: newY,
+					remaining: node.remaining / l,
+					children: []
+				};
+				node.children.push(child)
+			}
+		}
+		else{
+			// console.log("finishing node ("+ node.x +", "+ node.y+"), parentSeries.name = "+ parentSeries.name+
+			// 	", pathSeries.name = "+ pathSeries.name +", createNewSeries =  "+ createNewSeries);
+		}
+
+		// // create bucketed counts of new endings for this step in the path (newY -> newYObj)
+		// for(var i=0; i < remaining; i++){
+		// 	var randVelocity = _.sample(velocities);
+		// 	var newY = current.y + randVelocity;
+
+		// 	// clip to the target scope
+		// 	if(newY > targetScope){
+		// 		newY = targetScope;
+		// 	}
+
+		// 	// create the obj if necessary
+		// 	var newYObj = newYs[newY] = newYs[newY] || {
+		// 		y: newY,
+		// 		count: 0,
+		// 		parents: []
+		// 	};
+		// 	newYObj.count++;
+		// 	newYObj.parents = _.union(newYObj.parents, [current]);
+		// }
+
+
+		_.eachRight(node.children, function(child, i){
+			var newSeries = i != 0;
+			processNode(pathSeries, child, newSeries);
+		});
+	};
+
+	var dummyParentSeries = {
+		data: [],
+		name: 'dummy'
+	};
+	processNode(dummyParentSeries, projectionRoot, true);
+
+
+
+	/*
+
+	// iteration to process (outer array is iteration, inner is children to process in that iteration)
+	var iterationNodes = [projectionRoot];
+	var nextIterationNodes = [];
+	var newYs = {};
+
+	while(iterationNodes.length > 0){
+
+		var current = iterationNodes.pop();
 		var remaining = current.remaining;
 
 		// check if this path is done
 		if(current.y === targetScope){
 			// add it to the right histogram bucket
-			endCounts[current.x] = (endCounts[current.x] || 0) + 1;
-
-			// add it to the set of endings
-			pathEnds.push(current);
-
-			continue;
+			endCounts[current.x] = (endCounts[current.x] || 0) + current.remaining;
 		}
+		else{
 
-		// create bucketed counts of new endings for this step in the path (newY -> newYObj)
-		var newYs = {};
-		for(var i=0; i < remaining; i++){
-			var randVelocity = _.sample(velocities);
-			var newY = current.y + randVelocity;
+			// create bucketed counts of new endings for this step in the path (newY -> newYObj)
+			for(var i=0; i < remaining; i++){
+				var randVelocity = _.sample(velocities);
+				var newY = current.y + randVelocity;
 
-			// clip to the target scope
-			if(newY > targetScope){
-				newY = targetScope;
+				// clip to the target scope
+				if(newY > targetScope){
+					newY = targetScope;
+				}
+
+				// create the obj if necessary
+				var newYObj = newYs[newY] = newYs[newY] || {
+					y: newY,
+					count: 0,
+					parents: []
+				};
+				newYObj.count++;
+				newYObj.parents = _.union(newYObj.parents, [current]);
 			}
 
-			// create the obj if necessary
-			var newYObj = newYs[newY] = newYs[newY] || {
-				y: newY,
-				count: 0
-			};
-			newYObj.count++;
 		}
 
-		// convert buckets into new points to process
-		_.each(newYs, function(newYObj){
-			var child = {
-				x: current.x + 1,
-				y: newYObj.y,
-				remaining: newYObj.count,
-				parent: current,
-				children: []
-			}
-			current.children.push(child);
-			toProcess.push(child);
-		});
+		// check if it's time to move on to the next iteration
+		if(iterationNodes.length === 0){
+
+			// convert buckets into new points to process
+			_.each(newYs, function(newYObj){
+				var child = {
+					x: current.x + 1,
+					y: newYObj.y,
+					remaining: newYObj.count,
+					children: []
+				}
+
+				_.each(newYObj.parents, function(parent){
+					parent.children.push(child);
+				});
+				nextIterationNodes.push(child);
+			});
+
+			iterationNodes = nextIterationNodes;
+			nextIterationNodes = [];
+			newYs = {};
+		}
+
 	}
 
 	console.log('endCounts:', endCounts);
 
 
-	/*
-		Goal:
-
-		paths = [
-			{
-				x: 5,
-				y: 18,
-				runs: 10,000
-				paths: [
-					{
-						x: 6,
-						y:
-					}
-				]
-			}
-		]
-
-		// for histogram and finding path with max
-		endCounts = {
-			15 <iteration> : 100 <number of runs that ended in on this iteration
-		}
-
-		chart series
-		-------------
-
-		projectionStart = {
-			x: burnUp.length - 1,
-			y: burnUp[burnUp.length -1],
-		}
-
-		series[i] = {
-			data: [null x burnUp.length -1, projectionStart, projectionY1, projectionY2],
-			showInLegend: false, // unless Px
-			name: 'Px',
-			style: boldness based on number of peole on path at each step
-		}
-	*/
 
 
 	var series = [
@@ -148,32 +225,76 @@ $(function () {
 		}
 	];
 
-	// walk tree to convert paths to series
-	//TODO optimize to avoid overhead of repeated unshifts
-	_.each(pathEnds, function(end, i){
-		var data = [];
-		var current = end;
-		while(current != null){
-			data.unshift({
-				x: current.x,
-				y: current.y
-			});
-			current = current.parent;
+	var nextSeries = 1;
+
+	var processChild = function(parentSeriesData, child, createNewSeries){
+		var pathSeriesData;
+		if(createNewSeries){
+			var show = nextSeries < 3;
+			pathSeriesData = parentSeriesData;
+			var pathSeries = {
+				data: pathSeriesData,
+				name: 'Path '+ nextSeries++,
+				dashStyle: 'Dash',
+				color: '#FF8000',
+				marker: {
+					symbol: 'circle'
+				},
+				visible: show,
+				showInLegend: show
+
+			};
+			series.push(pathSeries);
+		}
+		else{
+			pathSeriesData = parentSeriesData;
 		}
 
-		var show = i % 5 === 0;
-		series.push({
-			data: data,
-			name: 'Path '+i,
-			dashStyle: 'Dash',
-			color: '#FF8000',
-			marker: {
-				symbol: 'circle'
-			},
-			showInLegend: show,
-			visible: show
+		pathSeriesData.push({
+			x: child.x,
+			y: child.y
 		});
-	});
+
+		var parentCopy = pathSeriesData.slice(0);
+
+		_.each(child.children, function(child, i){
+			var newSeries = i != 0;
+			parentSeriesData = newSeries ? parentCopy : pathSeriesData;
+			processChild(parentSeriesData, child, i != 0);
+		});
+	};
+
+	processChild([], projectionRoot, true);
+*/
+
+	// walk tree to convert paths to series
+	//TODO optimize to avoid overhead of repeated unshifts
+	// _.each(pathEnds, function(end, i){
+	// 	var data = [];
+	// 	var current = end;
+	// 	while(current != null){
+	// 		data.unshift({
+	// 			x: current.x,
+	// 			y: current.y
+	// 		});
+	// 		current = current.parent;
+	// 	}
+
+	// 	// var show = i % 5 === 0;
+	// 	var show = true;
+
+	// 	series.push({
+	// 		data: data,
+	// 		name: 'Path '+i,
+	// 		dashStyle: 'Dash',
+	// 		color: '#FF8000',
+	// 		marker: {
+	// 			symbol: 'circle'
+	// 		},
+	// 		showInLegend: show,
+	// 		visible: show
+	// 	});
+	// });
 
 	$('#container').highcharts({
 		chart: {
