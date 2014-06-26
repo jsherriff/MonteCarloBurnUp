6bfa5435eb38c4fa8080e5bd8a65866e8d8c6b17
Trever Shick <tshick@rallydev.com>
2014-06-26 16:33:56 -0400

    minor tweaks and refactorings

--- monteCarlo.js
+++ monteCarlo.js
@@ -1,141 +1,186 @@
 $(function () {
-	var calcVelocities = function(burnUp){
-		var velocities;
-		if(burnUp == null || burnUp.length === 0){
-			velocities = [1];
-		}
-		else if(burnUp.length === 1){
-			velocities = [ burnUp[0] ];
-		}
-		else{
-			velocities = [];
-			var prev = 0;
-			for(var i=0, l=burnUp.length; i < l; i++){
-				var burn = burnUp[i];
-				velocities.push(burn - prev);
-				prev = burn;
+	var seriesColor = "#0000ff";
+	var maximumNumberOfMonteCarloRuns = 500;
+
+
+
+
+	var query = function() {
+		return [];
+	}
+
+	var transform = function(data) {
+		var burnUp = [1, 5, 9, 15, 18];
+
+		/**
+		 * calculate the velocityies of the current iterations
+		 * @return an array of the velocity values
+		 */
+		var calcVelocities = function(burnUp){
+			var velocities;
+			if(burnUp == null || burnUp.length === 0){
+				velocities = [1];
 			}
+			else if(burnUp.length === 1){
+				velocities = [ burnUp[0] ];
+			}
+			else{
+				velocities = [];
+				var prev = 0;
+				for(var i=0, l=burnUp.length; i < l; i++){
+					var burn = burnUp[i];
+					velocities.push(burn - prev);
+					prev = burn;
+				}
+			}
+			return velocities;
+		};
+
+		return {
+			targetScope : 40,
+			iterationsComplete : burnUp.length,
+			pointsComplete : burnUp,
+			velocities: calcVelocities(burnUp)
+		};
+	}
+
+	var shadeColor = function(color, percent) {
+		var num = parseInt(color.slice(1),16), amt = Math.round(2.55 * percent), R = (num >> 16) + amt, G = (num >> 8 & 0x00FF) + amt, B = (num & 0x0000FF) + amt;
+		return "#" + (0x1000000 + (R<255?R<1?0:R:255)*0x10000 + (G<255?G<1?0:G:255)*0x100 + (B<255?B<1?0:B:255)).toString(16).slice(1);
+	};
+	var calculateColorToUse = function(iterationIndex, mostLikelyIterationIndex) {
+		var shadePercent = Math.abs(mostLikelyIterationIndex-iterationIndex) * 20;
+		var colorToUse = shadeColor(seriesColor, shadePercent);
+		// console.log("Shade percent", shadePercent);
+		// console.log("Shade color", colorToUse);
+		if (iterationIndex == mostLikelyIterationIndex) {
+			colorToUse = "purple";
 		}
-		return velocities;
-	};
+		return colorToUse;
+	}
+	var calculateDashStyleToUse = function(iterationIndex, mostLikelyIterationIndex) {
+		var dashStyle = "Dot";
+		if (iterationIndex == mostLikelyIterationIndex) {
+			dashStyle = "purple";
+		}
+		return dashStyle;
+	}
 
 
-	var burnUp = [1, 5, 9, 15, 18];
-	var targetScope = 40;
+
+
+
+
+	var dataToVisualize = transform(query());
 
 
 
 
 
-	var velocities = calcVelocities(burnUp);
-	console.log("Velocities", velocities)
 
-	var maxIterations = 20;
 
-	var noRuns = 500;
-
+	// run the simulation
 	var possibilities = [];
-	var endCounts = {};
-
-	var start = {
-		x: burnUp.length - 1,
-		y: burnUp[burnUp.length -1]
-	};
-
-
 	var maxX = 0;
 
-	for(var i=0; i < noRuns; i++){
+	var start = {
+		iteration: dataToVisualize.iterationsComplete - 1,
+		pointsComplete: dataToVisualize.pointsComplete[dataToVisualize.iterationsComplete - 1]
+	};
+
+
+	for(var i=0; i < maximumNumberOfMonteCarloRuns; i++){
 		var current = start;
 		var path = [current];
 
 		// keep going until we get there or give up hope
-		while(current.y < targetScope && current.x < maxIterations){
+		while(current.pointsComplete < dataToVisualize.targetScope) {//} && current.x < maxIterations){
 
-			var randVelocity = _.sample(velocities);
-			var newY = current.y + randVelocity;
+			var randVelocity = _.sample(dataToVisualize.velocities);
+			var newY = current.pointsComplete + randVelocity;
 
 			// clip to the target scope
-			if(newY > targetScope){
-				newY = targetScope;
+			if(newY > dataToVisualize.targetScope){
+				newY = dataToVisualize.targetScope;
 			}
 
 			current = {
-				x: current.x +1,
-				y: newY
+				iteration: current.iteration +1,
+				pointsComplete: newY
 			}
 			path.push(current);
 
-			if(current.y === targetScope){
-				//var pathStr = _.pluck(path, 'y').join('/');
+			if(current.pointsComplete === dataToVisualize.targetScope){
 				possibilities.push(path);
-				maxX = Math.max(maxX, current.x);
-				// pathMap[pathStr] = pathMap[pathStr] || path;
-				endCount = endCounts[current.x] = endCounts[current.x] || {
-					x: current.x,
-					y: 0
-				};
-				endCount.y++;
+				maxX = Math.max(maxX, current.iteration);
 			}
 		}
 	}
+	// end the simulation
 
-	console.log('endCounts:', endCounts);
 
 
 	// build histogram data
-	var countsByIteration = _.map(Array(maxIterations), function() { return 0; });
+	var countsByIteration = _.map(Array(maxX), function() { return 0; });
 	_.each(possibilities, function(possibility, idx){
 		// update the count by getting the last iteration in the
 		// possibility and incrementing that slot
-		countsByIteration[_.last(possibility).x]++
+		countsByIteration[_.last(possibility).iteration]++
 	});
+
+	// determine with iteration (index) has the highest frequency
 	var mostLikelyIteration = _.indexOf(countsByIteration, _.max(countsByIteration));
 
-	console.log("countsByIteration",countsByIteration);
-	console.log("maxX", maxX);
-	console.log("mostLikelyIteration",mostLikelyIteration);
 
 
 
 
-	var series = [
+	var todaysIterationIndex = dataToVisualize.iterationsComplete - 1;
+	var burnupSeriesData = [
 		{
-			data: burnUp,
-			name: 'Actual',
-			color: '#FF0000'
+			data: dataToVisualize.pointsComplete,
+			name: 'Actual'
 		}
 	];
 
 
 
-	var lastIterIndex = maxX;
-	console.log('lastIterIndex: ', lastIterIndex);
-
-	var maxIter = _.max(endCounts, 'y');
-	console.log('maxIter: ', maxIter);
-
-	// TODO - we need to show the min and the max as well.
-	// TODO - color the series by frequency
 	var showPathsModulo = 50;
-	var nextSeries = 1;
 
-	_.each(possibilities, function(path, i){
-		if ((i % showPathsModulo) !== 0) { return; }
 
-		var endsOnMax = (_.last(path).x == mostLikelyIteration);
+	// the number of series showing per iteration. this is used to
+	// ensure we dont' show too many
+	var seriesCountPerIteration = _.map(Array(maxX), function() { return 0; });
+	_.each(possibilities, function(path, i) {
 
-		_.last(path).marker = {
+		var iteration = _.last(path).iteration;
+
+		// let's ensure we show one line for each iteration
+		if (seriesCountPerIteration[iteration] > 0 && i % showPathsModulo !== 0) return;
+		seriesCountPerIteration[iteration]++;
+
+		var endsOnMax = (_.last(path).iteration == mostLikelyIteration);
+
+		var seriesData = _.map(path, function(pathPoint, idx){
+			return {
+				x: pathPoint.iteration,
+				y: pathPoint.pointsComplete
+			}
+		});
+
+		_.last(seriesData).marker = {
 			enabled: true,
 			symbol: endsOnMax ? 'circle' : 'triangle',
 			radius: 4
 		}
 
-		series.push({
-			data: path,
-			name: 'Path '+ nextSeries++,
-			dashStyle: 'Dash',
-			color: endsOnMax ? '#0000FF' : '#FF8000',
+		var colorToUse = calculateColorToUse(iteration, mostLikelyIteration);
+		var dashStyle = calculateDashStyleToUse(iteration, mostLikelyIteration);
+
+		burnupSeriesData.push({
+			data: seriesData,
+			dashStyle: dashStyle,
+			color: colorToUse,
 			marker: {
 				symbol: 'circle',
 				radius: 2
@@ -152,43 +197,52 @@ $(function () {
 
 
 
+	// both charts
+	var xAxisCategories = _.map(Array(maxX), function(v,i) { return i; });
 
 
+	var histogramSeries = _.map(countsByIteration, function(frequency,iteration) {
+		var shadePercent = Math.abs(mostLikelyIteration-iteration) * 20;
+		var colorToUse = calculateColorToUse(iteration, mostLikelyIteration)
 
+		return { x: iteration, y: frequency , color: colorToUse };
+	});
 
-
-
-	var createXAxisLabels = function(maxIterations){
-		var iterations = [];
-		for(var i=1; i < maxIterations; i++){
-			iterations.push('' + i);
-		}
-		return iterations;
-	};
-	var iterations = createXAxisLabels(maxIterations);
 
 
 
 	$('#burnup').highcharts({
-		chart: {
-			zoomType: 'xy'
+		chart : {
+			marginLeft:100,
+			marginTop:5
 		},
 		title: {
 			text: ''
 		},
 		xAxis: {
-			categories: iterations,
+			// categories: iterations,
 			min: 0,
-			max: lastIterIndex,
+			max: maxX,
+			categories: xAxisCategories,
 			plotLines: [
 				{
 					color: '#00AAFF',
-					value: start.x,
+					value: todaysIterationIndex,
 					width: 2,
 					label: {
 						text: 'TODAY',
 						style: {
 							color: '#00AAFF'
+						}
+					}
+				}, {
+					value: mostLikelyIteration,
+					width: 2,
+					color: calculateColorToUse(mostLikelyIteration,mostLikelyIteration),
+					label: {
+						text: 'PROBABLE FINISH',
+						style: {
+							color: calculateColorToUse(mostLikelyIteration,mostLikelyIteration)
 						}
 					}
 				}
@@ -198,82 +252,87 @@ $(function () {
 			}
 		},
 		yAxis: {
-			title: {
-				text: 'Count'
-			},
-			max:targetScope,
+			// title: {
+			// 	text: 'Count'
+			// },
+			max: dataToVisualize.targetScope,
 			min: 0
 		},
-		series: series,
-		credits: {
-			enabled: false
+		series: burnupSeriesData,
+		credits : {
+			enabled:false
 		}
+
 	});
 
-	var end = _.map(endCounts, function(count, iterIndex){
-		return {
-			x: parseInt(iterIndex, 10),
-			y: count
-		};
-	});
 
-	var histogramPoints = _.values(endCounts);
-	console.table(histogramPoints);
 
-	var histogramSeries = [
-		{
-			name: 'Counts',
-			data: histogramPoints
-		}
-	];
 
-	var histogramEnds = iterations.slice(0);
-	console.log("Histogram Ends", histogramEnds);
+
+
+
+
+
 
 	$('#histogram').highcharts({
-
 		chart: {
 			type: 'column',
-			zoomType: 'xy'
+			marginLeft:100,
+			marginBottom:0
+		},
+		tooltip : {
+			enabled:false
 		},
 		title: {
 			text: ''
 		},
 		plotOptions: {
-			column: {
-				borderWidth: 1,
-				pointPadding: 0,
-				groupPadding: 0
+			column : {
+				dataLabels: {
+					formatter: function() { return this.y == 0 ? '' : this.y ;},
+					enabled:true
+				}
 			}
 		},
 		legend: {
 			enabled: false
 		},
 		xAxis: {
+			// showFirstLabel: false,
 			labels: {
 				enabled:false
 			},
-			lineWidth: 0,
-			minorGridLineWidth:0,
-			lineColor:'transparent',
-			minorTickLength:0,
-			tickLength:0,
-			categories: histogramEnds,
+			// lineColor:'transparent',
+			categories: xAxisCategories,
 			min: 0,
-			max: lastIterIndex
+			max: maxX,
+			tickLength: 0
 		},
 		yAxis: {
-			min: 0,
-			title: {
-				text: ''
+			style: {
+				color: '#00AAFF'
 			},
-			labels: { enabled:false },
-			minorGridLineWidth:0,
-			gridLineWidth:0
+			showFirstLabel: false,
+			labels: {
+				style: {
+					color: '#00AAFF'
+
+				}
+				// rotation:90
+			},
+			title: {
+				text: 'Frequency'
+			},
+			min: 0
 		},
-		series: histogramSeries,
-		credits: {
-			enabled: false
+		series: [{
+			name: 'Counts',
+			color: '#00AAFF',
+
+			data: histogramSeries
+		}],
+		credits : {
+			enabled:false
 		}
 	});
 
